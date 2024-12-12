a10f5e9ba4f2dbe19952d01d2d7ba7ea94f80ccc
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-12-12 16:28:02 +0100

    CHANGE moved search function inside header instead of passing it using the context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-import { API_KEY, BASIC_URI } from './config/URL';
 // Pages
 import HomePage from './pages/HomePage';
 // Layouts
 import DefaultLayout from './layouts/DefaultLayout';
 // Contexts
 import GlobalContext from './contexts/GlobalContext';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
     const [movies, setMovies] = useState(null);
     const [tv, setTv] = useState(null);
     const [searchQuery, setSearchQuery] = useState('');
 
-    function search() {
-        if (searchQuery.trim() !== '') {
-            axios
-                .get(BASIC_URI + '/search/movie', {
-                    params: {
-                        api_key: API_KEY,
-                        query: searchQuery,
-                    },
-                })
-                .then((response) => {
-                    console.log(response);
-                    setMovies(response.data);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-            axios
-                .get(BASIC_URI + '/search/tv', {
-                    params: {
-                        api_key: API_KEY,
-                        query: searchQuery,
-                    },
-                })
-                .then((response) => {
-                    console.log(response);
-                    setTv(response.data);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-    }
-
     return (
-        <GlobalContext.Provider value={{ searchQuery, setSearchQuery, search }}>
+        <GlobalContext.Provider
+            value={{ searchQuery, setSearchQuery, setMovies, setTv }}
+        >
             <BrowserRouter>
                 <Routes>
                     <Route element={<DefaultLayout />}>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,44 @@
 import { useContext } from 'react';
+import axios from 'axios';
+import { BASIC_URI, API_KEY } from '../../config/URL';
 import GlobalContext from '../../contexts/GlobalContext';
 
 export default function Header() {
-    const { searchQuery, setSearchQuery, search } = useContext(GlobalContext);
+    const { searchQuery, setSearchQuery, setMovies, setTv } =
+        useContext(GlobalContext);
+
+    function search() {
+        if (searchQuery.trim() !== '') {
+            axios
+                .get(BASIC_URI + '/search/movie', {
+                    params: {
+                        api_key: API_KEY,
+                        query: searchQuery,
+                    },
+                })
+                .then((response) => {
+                    console.log(response);
+                    setMovies(response.data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+            axios
+                .get(BASIC_URI + '/search/tv', {
+                    params: {
+                        api_key: API_KEY,
+                        query: searchQuery,
+                    },
+                })
+                .then((response) => {
+                    console.log(response);
+                    setTv(response.data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }
 
     const handleChange = (event) => {
         setSearchQuery(event.target.value);
