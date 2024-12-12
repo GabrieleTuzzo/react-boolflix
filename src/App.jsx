d47714e086105d80d277ea03485b167aa84bfa6b
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-12-12 12:06:18 +0100

    ADD homepage routing and global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import GlobalContext from './contexts/GlobalContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Pages Imports
+import HomePage from './pages/HomePage';
 
 function App() {
-    return <></>;
+    return (
+        <GlobalContext.Provider>
+            <BrowserRouter>
+                <Routes>
+                    <Route index Component={HomePage}></Route>
+                </Routes>
+            </BrowserRouter>
+        </GlobalContext.Provider>
+    );
 }
 
 export default App;
