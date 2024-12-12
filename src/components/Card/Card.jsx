8e66e18b0cc313185ce3cbb41e131a4409d9ef22
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-12-12 16:09:13 +0100

    ADD used an object lookup instead of switch case for better readability inside Card component and adopted a better stars printing function for the votes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,60 @@
 import { IMG_URI } from '../../config/URL';
 
 export default function Card({ title, ogTitle, lang, vote, posterPath }) {
-    let src = null;
-    switch (lang) {
-        case 'it':
-            src = '/flags/it.svg';
-            break;
-        case 'fr':
-            src = '/flags/fr.svg';
-            break;
-        case 'de':
-            src = '/flags/de.svg';
-            break;
-        case 'en':
-            src = '/flags/en.svg';
-            break;
-        default:
-            src = '';
-    }
+    // let src = null;
+    let fullStarsNumber = Math.ceil(vote / 2);
+    const starsArray = Array.from({ length: 5 });
+
+    // switch (lang) {
+    //     case 'it':
+    //         src = '/flags/it.svg';
+    //         break;
+    //     case 'fr':
+    //         src = '/flags/fr.svg';
+    //         break;
+    //     case 'de':
+    //         src = '/flags/de.svg';
+    //         break;
+    //     case 'en':
+    //         src = '/flags/en.svg';
+    //         break;
+    //     default:
+    //         src = '';
+    // }
+
+    const flagLookup = {
+        it: '/flags/it.svg',
+        fr: '/flags/fr.svg',
+        de: '/flags/de.svg',
+        en: '/flags/en.svg',
+    };
 
     return (
         <div>
-            <img src={IMG_URI + '/w342' + posterPath}></img>
+            <img
+                src={IMG_URI + '/w342' + posterPath}
+                alt={`${title} poster`}
+            ></img>
             <h3>{title}</h3>
             <h4>{ogTitle}</h4>
-            {src ? <img src={src} alt="flag" width="30px"></img> : lang}
-            <p>{vote}</p>
+            {flagLookup[lang] ? (
+                <img
+                    src={flagLookup[lang]}
+                    alt={`${lang} flag`}
+                    width="30px"
+                ></img>
+            ) : (
+                <p>{lang}</p>
+            )}
+            <p>
+                {starsArray.map((_, i) =>
+                    i < fullStarsNumber ? (
+                        <i key={i} className="fa-solid fa-star"></i>
+                    ) : (
+                        <i key={i} className="fa-regular fa-star"></i>
+                    )
+                )}
+            </p>
         </div>
     );
 }
